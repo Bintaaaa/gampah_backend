34984b4bfec930acc63463b7f4114ea0e72c528d
Bintaaaa <bijantium.yt.32@gmail.com>
2021-12-01 20:42:45 +0700

    create migrate for table users

---
+++ database/migrations/2021_12_01_121925_add_field_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->date('last_active')->nullable()->after('password');
+            $table->boolean('user_status')->default(1)->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('last_active');
+            $table->dropColumn('user_status');
+        });
+    }
+}

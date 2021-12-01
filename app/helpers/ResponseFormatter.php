10c44f05de5af10893355917be1e0609d8f0ba0a
Bintaaaa <bijantium.yt.32@gmail.com>
2021-12-01 11:05:30 +0700

    response formater and give session for 3 days

---
+++ app/helpers/ResponseFormatter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * Format response.
+ */
+class ResponseFormatter
+{
+    /**
+     * API Response
+     *
+     * @var array
+     */
+    protected static $response = [
+        'meta' => [
+            'code' => 200,
+            'status' => 'success',
+            'message' => null,
+        ],
+        'data' => null,
+    ];
+
+    /**
+     * Give success response.
+     */
+    public static function success($data = null, $message = null)
+    {
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+
+    /**
+     * Give error response.
+     */
+    public static function error($data = null, $message = null, $code = 400)
+    {
+        self::$response['meta']['status'] = 'error';
+        self::$response['meta']['code'] = $code;
+        self::$response['meta']['message'] = $message;
+        self::$response['data'] = $data;
+
+        return response()->json(self::$response, self::$response['meta']['code']);
+    }
+}

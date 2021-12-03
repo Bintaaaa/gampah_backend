6aabf3cd933574686b6e9b8c529559c38213f63f
Seiko Santana <santanaaseiko@gmail.com>
2021-12-04 00:09:44 +0700

    Minor fix and added transaction creation

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ResponseFormatter;
 use Illuminate\Http\Request;
 use App\Models\transactions;
 use App\Models\User;
@@ -10,22 +11,49 @@ use Illuminate\Support\Facades\DB;
 class TransactionController extends Controller
 {
 
-    function getLeastBusyDriver()
+    static function getLeastBusyDriver()
     {
         if (transactions::count() == 0) {
             return User::where('roles', '=', 'driver')->first();
         } else {
             $joblessDrivers = DB::table("users")->select()->whereNotIn('users.id', DB::table("transactions")->select('driver_id'))->where('roles', '=', 'driver')->get();
             if ($joblessDrivers->count() == 0) {
-                $leastBusyDriverId = DB::table("users")->join('transactions', 'users.id', "=", "transactions.driver_id")
-                    ->select('transactions.driver_id', DB::raw('COUNT(transactions.driver_id) as jobcount'))
-                    ->groupBy('transactions.driver_id')
+                $leastBusyDriver = DB::table("users")->join('transactions', 'users.id', "=", "transactions.driver_id")
+                    ->select('users.id', DB::raw('COUNT(transactions.driver_id) as jobcount'))
+                    ->groupBy('users.id')
                     ->orderBy('jobCount', 'asc')
-                    ->first()->driver_id;
-                return User::where('id', '=', $leastBusyDriverId)->get();
+                    ->first();
+                return User::where('id', '=', $leastBusyDriver->id)->first();
             } else {
                 return $joblessDrivers;
             }
+        }
+    }
+
+    function create(Request $request)
+    {
+        $resultFileName = $request->file('report_img')->store('report_images');
+        $reporterId = $request->reporterId;
+        $driverId = TransactionController::getLeastBusyDriver()->id;
+        $reportImage = $resultFileName;
+        $addressDetail = $request->address_detail;
+        $status = "PENDING";
+        $latitude = $request->latitude;
+        $longitude = $request->longitude;
+
+        try {
+            transactions::create([
+                'reporter_id' => $reporterId,
+                'driver_id' => $driverId,
+                'report_image' => $reportImage,
+                'address_detail' => $addressDetail,
+                'status' => $status,
+                'longitude' => $longitude,
+                'latitude' => $latitude
+            ]);
+            ResponseFormatter::success(null, 'Transaction Created');
+        } catch (\Throwable $th) {
+            ResponseFormatter::error($th, 'Unable to create transaction');
         }
     }
 }
